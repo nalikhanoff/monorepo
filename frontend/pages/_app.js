458fdc5b9d933e5996f309bca664bca0b64d8c5c
nursultan <nalikhanoff@gmail.com>
2023-08-08 18:47:13 +0600

    initial commit on frontend

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -3,6 +3,13 @@ import Head from 'next/head';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider } from '@emotion/react';
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
 import createEmotionCache from '../config/createEmotionCache';
 import theme from '../config/theme';
 
@@ -21,6 +28,16 @@ function MyApp({
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                ErKul Company
+              </Typography>
+              <Button color="inherit">Login</Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
         <Component {...pageProps} />
       </ThemeProvider>
     </CacheProvider>
